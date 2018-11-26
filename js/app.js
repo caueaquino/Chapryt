9bb7434df0832e970ccf3a8d4b9642c97f1f1742
caueaquino <cauenogueira@alunos.utfpr.edu.br>
2018-11-26 00:54:34 -0200

    inicial commit, inicial frame

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+
+var config = {
+    apiKey: "AIzaSyAl_F5vLB1c37Q03W2GWKu7ZEftbPAtVdE",
+    authDomain: "chapryt-459b3.firebaseapp.com",
+    databaseURL: "https://chapryt-459b3.firebaseio.com",
+    projectId: "chapryt-459b3",
+    storageBucket: "chapryt-459b3.appspot.com",
+    messagingSenderId: "223892195945"
+};
+firebase.initializeApp(config);
