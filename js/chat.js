5ab771ba63d1986e9b58d3c2613b7abbab47f42c
caueaquino <cauenogueira@alunos.utfpr.edu.br>
2018-11-27 16:33:12 -0200

    fixed more bugs

--- js/chat.js
+++ js/chat.js
@@ -7,31 +7,38 @@ var users=[];
 var chats=[];
 
 var chat={
-    text: ""
+    text: "",
+    id: 0
 }
 
 //update the chat data with database in real time
 firebase.database().ref('chats').on('value', function (snapshot){
     snapshot.forEach(function(item){
-        chats.push(item.val());
+        chats=item.val();
         
-        var d=document.createElement("div");
-        var p=document.createElement("p");
+        if(document.getElementById(chats[chats.length-1].id)==null){
+            
+            var d=document.createElement("div");
+            var p=document.createElement("p");
 
-        d.className="container";
-        p.textContent=chats[chats.length-1];
+            d.id=chats[chats.length-1].id;
+            d.className="container";
+            p.textContent=chats[chats.length-1].text;
 
-        d.appendChild(p);
+            d.appendChild(p);
 
-        var di=document.getElementById("chatt");
+            var di=document.getElementById("chatt");
 
-        di.appendChild(d);
+            di.appendChild(d);
+        }
     });
 });
 
 function sendMessage(){
     chat.text="Someone said: "+document.getElementById("userMessage").value;
-    firebase.database().ref().child('chats').push(chat.text);
+    chat.id=chats.length;   
+    chats.push(chat);
+    firebase.database().ref().child('chats').push(chats);
 }
 
 function logout(){
